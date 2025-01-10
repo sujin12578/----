7b83c79308db4de918378394b10150559345e964
UserName <email@naver.com>
2025-01-10 15:19:40 +0900

    테스트 커밋

--- 로또 개념.py
+++ 로또 개념.py
@@ -11,3 +11,15 @@ abc = [1,2,3,4,5] # 데이터를 썩어 넣음
 #print(abc)
 
 random.choice(abc)
+menu = ('쫄면', '육개장', '비빔밥')
+random.choice(menu)
+random.choice([True, False])
+
+# 1. 리스트로 뽑아낼 것
+# 2. 중복을 제거할 것
+# 3. 로직 작성 시 for문으로 리스트에서 제공하는 함수 등으로 구성할 것것
+lotto = LottoNum()
+
+print(lotto.get_lotto_nums()) # [1,2,3,4,5,6]
+
+# 추가
